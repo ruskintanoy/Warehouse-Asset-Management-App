d375feddc9abbfd49d0a977fe0ec23653d2bd9d8
ruskintanoy <ruskintanoyy@gmail.com>
2026-03-16 12:03:23 -0600

    added material list data source

---
+++ my-app/.power/schemas/appschemas/dataSourcesInfo.ts
@@ -0,0 +1,15 @@
+/*!
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * This file is auto-generated. Do not modify it manually.
+ * Changes to this file may be overwritten.
+ */
+
+export const dataSourcesInfo = {
+  "material_desc": {
+    "tableId": "material_desc",
+    "version": "v2",
+    "primaryKey": "id",
+    "dataSourceType": "Connector",
+    "apis": {}
+  }
+};
